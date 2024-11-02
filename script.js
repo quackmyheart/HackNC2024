5b1d1bfadd5b835fada9492f7408b88443888e19
quackmyheart <85214314+quackmyheart@users.noreply.github.com>
2024-11-02 17:22:25 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ console.log("hello world"); //check if it's working
 // get the subreddit from the user 
 let subreddit_name = " "; 
 var button = document.getElementById("confirm_subreddit"); 
-button..addEventListener("click", function(event){
+button.addEventListener("click", function(event){
   subreddit_name = document.getElementById("subreddit_input").value;
 }); 
 console.log(subreddit_name, "button reddit subreddit"); // checking if the name is corrrect 
