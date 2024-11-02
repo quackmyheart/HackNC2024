31d57384a8aadaf7de0c616e80e66b7da4ecbd03
quackmyheart <85214314+quackmyheart@users.noreply.github.com>
2024-11-02 17:35:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 window.onload=function(){
- console.log("hello world"); //check if it's working 
-
-// get the subreddit from the user and put it in the label
-let subreddit_name = " "; 
-var test_label = document.getElementById("test_output"); // the test label to put things in 
-var button = document.getElementById("confirm_subreddit"); 
-button.addEventListener("click", getSubredditName);
-function getSubredditName(event) {
-  subreddit_name = document.getElementById("subreddit_input").value;
-  test_label.value = subreddit_name; 
-  console.log("yay"); 
-}
-
-console.log(subreddit_name, "button reddit subreddit"); // checking if the name is corrrect  
+ console.log("hello world"); //check if it's working
+ // get the subreddit from the user and put it in the label
+ let subreddit_name = " "; 
+ var test_label = document.getElementById("test_output"); // the test label to put things in 
+ var button = document.getElementById("confirm_subreddit"); 
+ button.addEventListener("click", getSubredditName);
+ function getSubredditName(event) {
+   subreddit_name = document.getElementById("subreddit_input").value;
+   test_label.value = subreddit_name; 
+   console.log("yay"); 
+ }
+ console.log(subreddit_name, "button reddit subreddit"); // checking if the name is corrrect  
 }
 
 
