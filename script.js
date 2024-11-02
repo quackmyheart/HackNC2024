91bc87dd01814ac2211708dffbb18f89f4735e2c
quackmyheart <85214314+quackmyheart@users.noreply.github.com>
2024-11-02 17:28:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 console.log("hello world"); //check if it's working 
 
-// get the subreddit from the user 
+// get the subreddit from the user and put it in the label
 let subreddit_name = " "; 
+var test_label = document.getElementById("test_output"); // the test label to put things in 
 var button = document.getElementById("confirm_subreddit"); 
 button.addEventListener("click", function(event){
   subreddit_name = document.getElementById("subreddit_input").value;
+  console.log("yay"); 
 }); 
 console.log(subreddit_name, "button reddit subreddit"); // checking if the name is corrrect 
 
