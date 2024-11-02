19f051af890eeb98a5549cc457ce0aa10be9f689
quackmyheart <85214314+quackmyheart@users.noreply.github.com>
2024-11-02 18:00:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-console.log("hello world"); //check if it's working
- // get the subreddit from the user and put it in the label
- let subreddit_name = " "; 
- const test_label = document.getElementById("test_output"); // the test label to put things in 
- const button = document.getElementById("confirm_subreddit"); 
- button?.addEventListener("click", getSubredditName);
- function getSubredditName(event) {
-   subreddit_name = document.getElementById("subreddit_input").value;
-   test_label.value = subreddit_name; 
-   console.log("yay"); 
- }
- console.log(subreddit_name, "button reddit subreddit"); // checking if the name is corrrect  
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("hello world"); //check if it's working
+   // get the subreddit from the user and put it in the label
+   let subreddit_name = " "; 
+   const test_label = document.getElementById("test_output"); // the test label to put things in 
+   const button = document.getElementById("confirm_subreddit"); 
+   button?.addEventListener("click", getSubredditName);
+   function getSubredditName(event) {
+     subreddit_name = document.getElementById("subreddit_input").value;
+     test_label.value = subreddit_name; 
+     console.log("yay"); 
+   }
+   console.log(subreddit_name, "button reddit subreddit"); // checking if the name is corrrect  
+  
+  // get something from the reddit json
+  // for example https://www.reddit.com/r/ruby.json is for r/ruby 
+  // "https://www.reddit.com/r/" + subreddit_name + ".json"
+  var subredddit_json = " "; 
+  
+   // display nicely 
 
-// get something from the reddit json
-// for example https://www.reddit.com/r/ruby.json is for r/ruby 
-// "https://www.reddit.com/r/" + subreddit_name + ".json"
-var subredddit_json = " "; 
-
- // display nicely 
-
+});
 
 
 
