647d54df53ffe6ba9930deca97b7625166181d71
quackmyheart <85214314+quackmyheart@users.noreply.github.com>
2024-11-02 17:46:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
  console.log("hello world"); //check if it's working
  // get the subreddit from the user and put it in the label
  let subreddit_name = " "; 
- var test_label = document.getElementById("test_output"); // the test label to put things in 
+ const test_label = document.getElementById("test_output"); // the test label to put things in 
  const button = document.getElementById("confirm_subreddit"); 
  button.addEventListener("click", getSubredditName);
  function getSubredditName(event) {
