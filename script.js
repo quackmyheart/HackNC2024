ff719ed8770ce2f05e4279351062c0f605d8854c
quackmyheart <85214314+quackmyheart@users.noreply.github.com>
2024-11-02 17:18:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 console.log("hello world");
 
 // get the subreddit 
-let subreddit_name = document.getElementById("").value;
+let subreddit_name = " "; 
+
+var button = document.getElementById("confirm_subreddit"); 
+button..addEventListener("click", function(event){
+  subreddit_name = document.getElementById("subreddit_input").value;
+}); 
 console.log(subreddit_name); 
 
