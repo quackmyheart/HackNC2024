7205e4a074ee61e02e270c02a520917c28e68ccb
quackmyheart <85214314+quackmyheart@users.noreply.github.com>
2024-11-02 17:20:18 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,9 @@ console.log("hello world");
 
 // get the subreddit 
 let subreddit_name = " "; 
-
 var button = document.getElementById("confirm_subreddit"); 
 button..addEventListener("click", function(event){
   subreddit_name = document.getElementById("subreddit_input").value;
 }); 
-console.log(subreddit_name); 
+console.log(subreddit_name, "button reddit subreddit"); // checking if the name is corrrect 
 
